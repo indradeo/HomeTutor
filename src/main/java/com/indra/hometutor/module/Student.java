b132d9317b5a4ba8dd32b8b49e62dab1bc313549
Indradeo Kumar <indradeokumar1234@gmail.com>
2023-10-07 01:08:11 +0530

    Setup diployement things

--- src/main/java/com/indra/hometutor/module/Student.java
+++ src/main/java/com/indra/hometutor/module/Student.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(schema = "hometutor",name = "student")
+@Table
 public class Student {
 	
 	
--- src/main/java/com/indra/hometutor/module/Tutor.java
+++ src/main/java/com/indra/hometutor/module/Tutor.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(schema = "hometutor",name = "tutor")
+@Table
 public class Tutor {
 	
 	@Id 
